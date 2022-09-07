2e3be450e46d779cb14d8d31b1d8ecc0b5aa68fc
Evgeniy-51 <evgeniy-51@yandex.ru>
2022-09-07 19:32:07 +0300

    task3

--- task2/task2.cs
+++ task2/task2.cs
@@ -3,26 +3,26 @@
 
 Console.Clear();
 
-string[] pName = new string[] { "k1", "b1", "k2", "b2" };
+string[] pointName = new string[] { "k1", "b1", "k2", "b2" };
 double[] points = new double[4];
 double resX,
        resY;
 
 for (int i = 0; i < 4; i++)
 {
-    System.Console.Write($"Введите координату {pName[i]}:  ");
+    Console.Write($"Введите координату {pointName[i]}:  ");
     points[i] = Convert.ToDouble(Console.ReadLine());
 }
 
 if (points[0] == points[2])
 {
-    System.Console.WriteLine("Прямые параллельны");
+    Console.WriteLine("Прямые параллельны");
 }
 else
 {
     resX = (points[3] - points[1]) / (points[0] - points[2]);
     resY = points[0] * resX + points[1];
-    System.Console.WriteLine($"Координаты пересечения: {resX}, {resY}");
+    Console.WriteLine($"Координаты пересечения: {resX}, {resY}");
 }
 
 
---
+++ task3/task3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+// * Найдите сумму произведений пар чисел в одномерном целочисленном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Если кол-во элементов нечетное, то элемент посередине не учитывать.
+// Результат сложения вывести на экран.
+// 1, 3, 8, 3, 2 -> 11
+// 8, 3, 4, 2 -> 22
+
+Console.Clear();
+
+Random rnd = new Random();
+int sum = 0;
+
+int size = rnd.Next(5, 10);
+int[] arr = new int[size];
+for (int i = 0; i < size; i++)
+{
+    arr[i] = rnd.Next(9);
+}
+
+for (int i = 0; i < size / 2; i++)
+{
+    sum += arr[i] * arr[size - 1 - i];
+}
+
+Console.WriteLine(String.Join(" ", arr));
+Console.WriteLine("Сумма произв. пар элементов= " + sum);
+
